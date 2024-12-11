f9ce44a0294afc54a2d1c6c2512128c221ff2a21
Mohamed Sofiene (Niemand) <chill.out.x19@gmail.com>
2024-12-11 14:58:05 +0100

    feat: :ambulance: add challenge 1 part 1

---
+++ challenges/1/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSolve(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		inputName := "test.input"
+		input, err := getInput(inputName)
+		assert.Nil(t, err)
+		require.Nil(t, err)
+
+		assert.NotEmpty(t, input)
+		l1, l2, err := getListsFromInput(input)
+		assert.Nil(t, err)
+		require.Nil(t, err)
+
+		assert.Equal(t, 6, len(l1))
+		assert.Equal(t, 6, len(l2))
+		assert.Equal(t, []int{1, 2, 3, 3, 3, 4}, l1)
+		assert.Equal(t, []int{3, 3, 3, 4, 5, 9}, l2)
+
+		distances := calculateDistances(l1, l2)
+		assert.Equal(t, 6, len(distances))
+		assert.Equal(t, []int{2, 1, 0, 1, 2, 5}, distances)
+
+		assert.Equal(t, 11, totalDistances(distances))
+	})
+}
