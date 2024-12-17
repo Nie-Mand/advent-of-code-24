9159d8c3d462fcf04654745a8ea1a20f4657535d
Mohamed Sofiene (Niemand) <chill.out.x19@gmail.com>
2024-12-17 16:41:44 +0100

    feat: :zap: solved day 5 part 1

--- challenges/5/part1/main.go
+++ challenges/5/part1/main.go
@@ -1,5 +1,148 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Rule struct {
+	first, next int
+}
+
+func r(first, next int) Rule {
+	return Rule{first: first, next: next}
+}
+
+type Set map[int]bool
+
+func set() Set {
+	return Set{}
+}
+
+func (s Set) add(i int) {
+	s[i] = true
+}
+
+func (s Set) has(i int) bool {
+	_, ok := s[i]
+	return ok
+}
+
+func loadInput(filename string) ([]Rule, [][]int, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return []Rule{}, [][]int{}, err
+	}
+
+	data := strings.Split(string(b), "\n")
+
+	rules := make([]Rule, 0, len(data))
+	updates := make([][]int, 0, len(data))
+
+	idx := 0
+	phase := 0
+	for idx < len(data) && phase < 2 {
+		line := data[idx]
+
+		if line == "" {
+			phase += 1
+			idx += 1
+			continue
+		}
+
+		switch phase {
+		case 0:
+			rule := Rule{}
+			fmt.Sscanf(line, "%d|%d\n", &rule.first, &rule.next)
+
+			rules = append(rules, rule)
+			break
+		case 1:
+			pages := strings.Split(line, ",")
+			update := make([]int, len(pages))
+			for i, page := range pages {
+				fmt.Sscanf(page, "%d", &update[i])
+			}
+
+			updates = append(updates, update)
+			break
+		}
+		idx++
+	}
+
+	return rules, updates, nil
+}
+
+type Index map[int]Set
+
+func index() Index {
+	return map[int]Set{}
+}
+
+func (idx Index) has(v int) bool {
+	_, ok := idx[v]
+	return ok
+}
+
+func (idx Index) add(first, second int) {
+	if _, ok := idx[first]; !ok {
+		idx[first] = set()
+	}
+
+	if _, ok := idx[second]; !ok {
+		idx[second] = set()
+	}
+
+	idx[first].add(second)
+}
+
+func (idx Index) before(first, second int) bool {
+	if !idx.has(second) || !idx.has(first) {
+		return true
+	}
+
+	return idx[first].has(second)
+}
+
+func (idx Index) indexRules(rules []Rule) {
+	for _, rule := range rules {
+		idx.add(rule.first, rule.next)
+	}
+}
+
+func (idx Index) rightOrder(pages []int) bool {
+	for i := 0; i < len(pages)-1; i++ {
+		if !idx.before(pages[i], pages[i+1]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func middle(pages []int) int {
+	if len(pages)%2 == 0 {
+		return pages[len(pages)/2-1]
+	}
+
+	return pages[len(pages)/2]
+}
+
+func solve(rules []Rule, updates [][]int) int {
+	idx := index()
+	idx.indexRules(rules)
+
+	result := 0
+	for _, update := range updates {
+		if idx.rightOrder(update) {
+			result += middle(update)
+		}
+	}
+
+	return result
+}
+
 func handleError(err error) {
 	if err != nil {
 		panic(err)
@@ -7,4 +150,9 @@ func handleError(err error) {
 }
 
 func main() {
+	filename := "puzzle.input"
+	rules, updates, err := loadInput(filename)
+	handleError(err)
+
+	fmt.Println(solve(rules, updates))
 }
--- challenges/5/part1/main_test.go
+++ challenges/5/part1/main_test.go
@@ -2,10 +2,61 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSolve(t *testing.T) {
 	t.Run("", func(t *testing.T) {
+		filename := "test.input"
+		rules, updates, err := loadInput(filename)
+		assert.Nil(t, err, "Error loading input")
+		require.Nil(t, err, "Error loading input")
 
+		assert.Equal(t, Rule{first: 47, next: 53}, r(47, 53), "Expected 21 rules")
+		assert.Equal(t, 21, len(rules), "Expected 21 rules")
+		assert.Equal(t, r(47, 53), rules[0], "Expected 21 rules")
+		assert.Equal(t, r(53, 13), rules[20], "Expected 21 rules")
+
+		assert.Equal(t, 6, len(updates), "Expected 6 updates")
+		assert.Equal(t, 5, len(updates[0]), "Expected 5 pages")
+		assert.Equal(t, 29, updates[0][4], "Expected page 29")
+
+		s := set()
+		assert.Equal(t, false, s.has(1), "set should not have 1")
+		s.add(1)
+		assert.Equal(t, true, s.has(1), "set should have 1")
+
+		idx := index()
+		assert.NotNil(t, idx, "can't create index")
+		assert.Equal(t, false, idx.has(47), "index should not have 47")
+		idx.add(47, 53)
+		idx.add(47, 11)
+		idx.add(11, 2)
+		idx.add(11, 2)
+		idx.add(53, 10)
+		assert.Equal(t, true, idx.has(47), "index should have 47")
+		assert.Equal(t, true, idx[47].has(53), "set should have 53")
+		assert.Equal(t, true, idx[47].has(11), "set should have 11")
+		assert.Equal(t, true, idx.has(53), "index should have 47")
+		assert.Equal(t, true, idx[53].has(10), "set should have 11")
+
+		assert.Equal(t, true, idx.before(47, 11), "47 should be before 11")
+		assert.Equal(t, true, idx.before(47, 15), "47 should be before 15")
+		assert.Equal(t, true, idx.before(28, 47), "28 should be before 47")
+		assert.Equal(t, true, idx.before(47, 4), "47 should be before 4")
+
+		idx = index()
+		assert.Equal(t, false, idx.has(53), "index should not have 53")
+		idx.indexRules(rules)
+		assert.Equal(t, true, idx.has(53), "index should have 53")
+		assert.Equal(t, true, idx.rightOrder([]int{5, 47, 61, 53, 29}), "5,47,61,53,29 should be in right order")
+		assert.Equal(t, false, idx.rightOrder([]int{75, 97, 47, 61, 53}), "75,97,47,61,53 should not be in right order")
+
+		assert.Equal(t, 61, middle([]int{75, 47, 61, 53, 29}), "Expected 61")
+		assert.Equal(t, 29, middle([]int{75, 29, 13}), "Expected 29")
+
+		assert.Equal(t, 143, solve(rules, updates), "Expected 143")
 	})
 }
