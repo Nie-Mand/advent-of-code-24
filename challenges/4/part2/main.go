a642502d96b3135f18506af7e571de26dd421f6e
Mohamed Sofiene (Niemand) <chill.out.x19@gmail.com>
2024-12-17 15:12:03 +0100

    feat: :fire: completed day 4

--- challenges/4/part2/main.go
+++ challenges/4/part2/main.go
@@ -1,5 +1,120 @@
 package main
 
+import (
+	"os"
+	"strings"
+)
+
+type P struct {
+	x, y int
+}
+
+func p(x, y int) P {
+	return P{x, y}
+}
+
+type Traversal struct {
+	input []string
+}
+
+func traverse(input []string) Traversal {
+	return Traversal{input}
+}
+
+func (t *Traversal) out(x, y int) bool {
+	bx := len(t.input[0])
+	by := len(t.input)
+
+	return x < 0 || y < 0 || x >= bx || y >= by
+}
+
+func (t *Traversal) at(x, y int) string {
+	if t.out(x, y) {
+		return ""
+	}
+
+	return string(t.input[y][x])
+}
+
+func (t *Traversal) allStarts() []P {
+	starts := []P{}
+	for y, row := range t.input {
+		for x := range row {
+			if t.qualifies(x, y) {
+				starts = append(starts, p(x, y))
+			}
+		}
+	}
+
+	return starts
+}
+
+func (t *Traversal) qualifies(x, y int) bool {
+	if t.at(x, y) != "A" {
+		return false
+	}
+
+	requiredPaths := []P{
+		p(x+1, y+1),
+		p(x-1, y-1),
+		p(x+1, y-1),
+		p(x-1, y+1),
+	}
+
+	for _, p := range requiredPaths {
+		if t.out(p.x, p.y) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (t *Traversal) works(x, y int) bool {
+	if !t.qualifies(x, y) {
+		return false
+	}
+
+	crossings := 0
+	rising := []P{p(x-1, y-1), p(x+1, y+1)}
+	falling := []P{p(x-1, y+1), p(x+1, y-1)}
+
+	if t.at(rising[0].x, rising[0].y) == "M" && t.at(rising[1].x, rising[1].y) == "S" {
+		crossings++
+	} else if t.at(rising[0].x, rising[0].y) == "S" && t.at(rising[1].x, rising[1].y) == "M" {
+		crossings++
+	}
+
+	if t.at(falling[0].x, falling[0].y) == "M" && t.at(falling[1].x, falling[1].y) == "S" {
+		crossings++
+	} else if t.at(falling[0].x, falling[0].y) == "S" && t.at(falling[1].x, falling[1].y) == "M" {
+		crossings++
+	}
+
+	return crossings == 2
+}
+
+func (t *Traversal) solve() int {
+	starts := t.allStarts()
+	count := 0
+	for _, p := range starts {
+		if t.works(p.x, p.y) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func loadInput(filename string) ([]string, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return strings.Split(strings.Trim(string(b), "\n"), "\n"), nil
+}
+
 func handleError(err error) {
 	if err != nil {
 		panic(err)
@@ -7,5 +122,9 @@ func handleError(err error) {
 }
 
 func main() {
-
+	filename := "puzzle.input"
+	input, err := loadInput(filename)
+	handleError(err)
+	t := traverse(input)
+	println(t.solve())
 }
--- challenges/4/part2/main_test.go
+++ challenges/4/part2/main_test.go
@@ -2,9 +2,40 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSolve(t *testing.T) {
 	t.Run("", func(t *testing.T) {
+		filename := "test.input"
+		input, err := loadInput(filename)
+		assert.Nil(t, err, "can't read input")
+		require.Nil(t, err, "can't read input")
+
+		miniFilename := "mini-test.input"
+		miniInput, err := loadInput(miniFilename)
+		assert.Nil(t, err, "can't read input")
+		require.Nil(t, err, "can't read input")
+
+		T := traverse(input)
+		Tm := traverse(miniInput)
+		assert.Equal(t, Tm.at(1, 1), "A")
+		assert.Equal(t, Tm.at(2, 2), "S")
+
+		assert.Equal(t, true, Tm.qualifies(1, 1), "can't qualify")
+		assert.Equal(t, false, Tm.qualifies(2, 1), "can't qualify")
+
+		assert.Equal(t, true, Tm.works(1, 1), "can't work")
+		assert.Equal(t, P{0, 0}, p(0, 0), "can't create a point")
+		assert.Equal(t, true, T.out(-1, 0), "can't check out of bounds")
+		assert.Equal(t, true, T.out(-1, 0), "can't check out of bounds")
+		assert.Equal(t, false, T.out(0, 1), "can't check out of bounds")
+		assert.Equal(t, true, T.out(0, 10), "can't check out of bounds")
+
+		assert.NotEmpty(t, len(T.allStarts()))
+		assert.Equal(t, 1, Tm.solve())
+		assert.Equal(t, 9, T.solve())
 	})
 }
